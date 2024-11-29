42dcd3066b736bbfd2fdab4fb50b26b4f9501d95
Homan Qiu <thehuiq2006@gmail.com>
2024-11-28 21:44:07 -0800

    uhh, I think i can do this by itself without unity's game engine, but mannn it's just so much nicer to work with a game engine

--- KillEldenRing/PythonTest.py
+++ KillEldenRing/PythonTest.py
@@ -4,17 +4,13 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtCore import Qt
 
-
-
-
-
-Value = int(open(r"C:\Users\NoahC\OneDrive\Desktop\Boss_2\Boss\Numbers.txt", "r").read())
+Value = int(open("ScoreCount.txt", "r").read())
 while True:
     try:
-        img = pyautogui.locateOnScreen(r'C:\Users\NoahC\OneDrive\Desktop\Boss_2\Boss\Boss.png', confidence=0.6, grayscale=True)
+        img = pyautogui.locateOnScreen('ScoreCount.txt', confidence=0.6, grayscale=True)
         Value += 1
 
-        File = open(r"C:\Users\NoahC\OneDrive\Desktop\Boss_2\Boss\Numbers.txt", "w")
+        File = open("ScoreCount.txt", "w")
         File.write(str(Value))
         File.close()
 
